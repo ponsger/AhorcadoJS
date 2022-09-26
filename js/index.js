d435a894572db8b950880fd6520b1ab9beb44cc5
ponsger <german.aopg@gmail.com>
2022-09-26 09:24:20 -0500

    Desplegado el contenido en js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,19 @@
 'use strict'
 
-console.log("hola");
+var bienvenida = document.getElementById("bienvenida");
+var durante_juego = document.getElementById("durante-juego");
+var nueva_palabra = document.getElementById("nueva-palabra");
+
+durante_juego.innerHTML="<canvas class='dibuja-ahorcado'></canvas>"+
+            "<div id='letras'>"+
+                "<div class='letras-correctas'>"+
+            "    </div>"+
+            "    <div class='letras-incorrectas'>"+
+            "    </div>"+
+            "</div>"+
+            "<button class='btn btn-azul'>Nuevo Juego</button>"+
+            "<button class='btn btn-blanco'>Desistir</button>";
+nueva_palabra.innerHTML="<textarea name='text-nueva-palabra' id='text-nueva-palabra' cols='30' rows='10' placeholder='Ingresa la nueva palabra'></textarea>"+
+"<p class='advertencia'>Máximo 8 palabras</p>"+
+"<button class='btn btn-azul'>Guardar y jugar</button>"+
+"<button class='btn btn-blanco'>Cancelar</button>";
